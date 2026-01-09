2736d2056a85fa0ac0483616dfe27b5ff72a3e9b
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-09 19:02:31 +0100

    ci: try to fix semantic-release module error

--- release.config.js
+++
@@ -1,6 +0,0 @@
-import config, { plugins } from 'semantic-release-preconfigured-conventional-commits';
-plugins.push(
-    "@semantic-release/github",
-    "@semantic-release/git",
-)
-export default config
