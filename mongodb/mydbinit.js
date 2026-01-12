780a6fb441d3889b4bf4473b8fcc8aae6e7dd9e4
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-12 11:01:55 +0100

    build: setup docker and mongoDB

---
+++ mongodb/mydbinit.js
@@ -0,0 +1,27 @@
+var conn = new Mongo();
+var db = conn.getDB('ChargeAndTrackDB');
+
+db.createCollection('users');
+db.createCollection('chargingStations');
+
+try {
+   db.users.deleteMany( { } );
+} catch (e) {
+   print (e);
+}
+
+var cursor = db.users.find();
+while ( cursor.hasNext() ) {
+   printjson( cursor.next() );
+}
+
+db.users.insertMany(
+   {"username:": "admin", "password": "admin1234", "role": "ADMIN"},
+   {"username": "user1", "password": "user11234", "role": "BASE_USER"},
+   {"username": "user2", "password": "user21234", "role": "BASE_USER"}
+);
+
+var cursor = db.users.find();
+while ( cursor.hasNext() ) {
+   printjson( cursor.next() );
+}
