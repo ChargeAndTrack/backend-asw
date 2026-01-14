657225cb32ab4bcd48366517675e95b23e380239
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-01-14 19:04:41 +0100

    feat(query-cars): add car schema and update user schema

--- nodejs/src/models/car.ts
+++ nodejs/src/models/car.ts
@@ -1,5 +1,13 @@
+import mongoose from 'mongoose'
+
 export interface Car {
     plate: string;
     maxBattery: number;
-    currentBattery: number;
+    currentBattery?: number;
 }
+
+export const carSchema = new mongoose.Schema<Car>({
+    plate: {type: String, required: true},
+    maxBattery: {type: Number, required: true},
+    currentBattery: {type: Number}
+});
--- nodejs/src/models/user.ts
+++ nodejs/src/models/user.ts
@@ -1,4 +1,5 @@
 import type { Car } from './car.ts';
+import { carSchema } from './car.ts';
 import mongoose from 'mongoose';
 
 export interface User {
@@ -15,10 +16,16 @@ export const Role = {
 
 export type Role = (typeof Role)[keyof typeof Role]
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    role: String
+const userSchema = new mongoose.Schema<User>({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    cars: { type: [carSchema], default: function () {
+        if (this.role === 'BASE_USER') {
+            return [];
+        }
+        return undefined;
+    }}
 });
 
 export const userModel = mongoose.model('User', userSchema);
