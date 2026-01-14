6be22b1750d70f0d4705dd6afc9dd07418a98c5f
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-14 19:50:31 +0100

    feat(query-charging-stations): add charging station schema and model

--- nodejs/src/models/chargingStation.ts
+++ nodejs/src/models/chargingStation.ts
@@ -1,7 +1,16 @@
 import type { Facility } from "./facility.ts";
+import mongoose from "mongoose";
 
 export interface ChargingStation extends Facility {
     power: number; // in kW
     available: boolean;
     enabled: boolean;
 }
+
+const chargingStationSchema = new mongoose.Schema<ChargingStation>({
+    power: { type: Number, required: true },
+    available: { type: Boolean, default: true },
+    enabled: { type: Boolean, default: true }
+}, { versionKey: false });
+
+export const chargingStationModel = mongoose.model('ChargingStation', chargingStationSchema);
