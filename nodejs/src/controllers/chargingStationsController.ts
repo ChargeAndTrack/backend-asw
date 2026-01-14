d246546c9acb92c46ba55f9c6f65a809e01920bf
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-14 19:55:09 +0100

    feat(query-charging-stations): implement listChargingStations and addChargingStation handlers

--- nodejs/src/controllers/chargingStationsController.ts
+++ nodejs/src/controllers/chargingStationsController.ts
@@ -3,14 +3,29 @@ import { chargingStationModel } from '../models/chargingStation.ts';
 
 // GET /chargingStations
 export const listChargingStations = async (req: Request, res: Response): Promise<Response> => {
-    console.log("Get charging stations");
-    return res.sendStatus(200);
+    try {
+        const chargingStation = await chargingStationModel.find();
+        return res.json(chargingStation);
+    } catch (error) {
+        return res.send(error)
+    }
 };
 
 // POST /chargingStations
 export const addChargingStation = async (req: Request, res: Response): Promise<Response> => {
-    console.log("Add charging station");
-    return res.sendStatus(200);
+    try {
+        console.log("Add charging station request: " + req.body.power);
+        const chargingStation = await chargingStationModel.insertOne({
+            power: req.body.power,
+            available: req.body.available,
+            enabled: req.body.enabled
+        });
+        console.log("Added charging station: " + chargingStation);
+        return res.sendStatus(200);
+    } catch (error) {
+        console.log("Fail adding a chargin station " + error);
+        return res.status(400).send("Fail adding a chargin station");
+    }
 };
 
 // GET /chargingStations/:id
