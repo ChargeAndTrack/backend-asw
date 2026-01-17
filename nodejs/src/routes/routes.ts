3fdcbe1c03a23be1e2bc7f4c810fa0592cf15c10
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-17 13:54:22 +0100

    feat(recharge): add start-recharge route and define its handler, change charging stations style in route paths, setup server with socket.io

--- nodejs/src/routes/routes.ts
+++ nodejs/src/routes/routes.ts
@@ -4,13 +4,14 @@ import * as loginController from "../controllers/loginController.ts";
 import * as chargingStationsController from "../controllers/chargingStationsController.ts";
 import * as carsController from "../controllers/carsController.ts";
 import * as locationController from "../controllers/locationController.ts";
+import * as rechargingController from "../controllers/rechargeController.ts";
 
 const router = Router();
 
 router.post('/login', loginController.login);
 router.get('/user', verifyLogin, getUser);
 
-router.route('/chargingStations')
+router.route('/charging-stations')
     .get(verifyLogin, chargingStationsController.listChargingStations)
     .post(verifyLogin, verifyAdminRole, chargingStationsController.addChargingStation);
 
@@ -19,10 +20,13 @@ router.get('/location/reverse', verifyLogin, locationController.reverseCoordinat
 router.get('/charging-stations/near', verifyLogin, chargingStationsController.getNearbyChargingStations);
 router.get('/charging-stations/closest', verifyLogin, chargingStationsController.getClosestChargingStation);
 
-router.route('/chargingStations/:id')
+router.route('/charging-stations/:id')
     .get(verifyLogin, chargingStationsController.getChargingStation)
     .put(verifyLogin, verifyAdminRole, chargingStationsController.updateChargingStation)
     .delete(verifyLogin, verifyAdminRole, chargingStationsController.removeChargingStation)
+
+router.route('/charging-stations/:id/start-recharge')
+    .post(verifyLogin, rechargingController.startRecharge);
 
 router.route('/cars')
     .get(verifyLogin, carsController.readUserCars)
