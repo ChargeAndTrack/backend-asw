6351809b480871333e2b4ace8c7ba98f6017917c
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-17 13:42:16 +0100

    chore(recharge): add redis configuration

--- nodejs/src/config/config.ts
+++ nodejs/src/config/config.ts
@@ -3,16 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
-  port: number;
-  jwtSecret: string;
+    port: number;
+    redisHost: string,
+    redisPort: number,
+    jwtSecret: string;
 }
 
 if (!process.env['JWT_SECRET']) {
-  throw new Error("Missing JWT_SECRET in .env file");
+    throw new Error("Missing JWT_SECRET in .env file");
 }
 const config: Config = {
-  port: Number(process.env['PORT']) || 3000,
-  jwtSecret: process.env['JWT_SECRET'],
+    port: Number(process.env['PORT']) || 3000,
+    redisHost: process.env['REDIS_HOST'] || 'redis',
+    redisPort: Number(process.env['REDIS_PORT']) || 6379,
+    jwtSecret: process.env['JWT_SECRET'],
 };
 
 export default config;
