d9dc09feaf0ee9195d1bf9ddf78f2ea114c6820a
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-01-26 13:37:25 +0100

    feat: add cors to express app

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes/routes.ts';
+import cors from 'cors';
 
 mongoose.connect('mongodb://mongodb:27017/ChargeAndTrackDB');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1', routes);
 
 export default app;
